3863415622efe3f63bca3e950614db6928b1a189
Otávio Bumaruf <OtavioBumaruf@gmail.com>
2020-06-18 18:45:50 -0300

    Alteração feita pelo Otávio

--- espelha-ai/Client/Program.cs
+++ espelha-ai/Client/Program.cs
@@ -17,7 +17,7 @@ namespace Client
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            // Implementacao feita pelo lucas
+            // Otávio
         }
     }
 }
