ace9adbb46d53ae26656bc3a3e04867f0aea6539
Lucas Adum <lucasadum@gmail.com>
2020-06-18 18:40:10 -0300

    Alterações

--- espelha-ai/Client/Program.cs
+++ espelha-ai/Client/Program.cs
@@ -17,7 +17,7 @@ namespace Client
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            // Lucas
+            // Implementacao feita pelo lucas
         }
     }
 }
